dce876c3e8268953f4cb2cc17ae24e5358592208
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-02-16 08:19:56 +0300

    feat: add types

---
+++ src/types/enums.ts
@@ -0,0 +1,14 @@
+export enum MESSAGE_TYPES {
+  REGISTRATION = 'reg',
+  UPDATE_WINNERS = 'update_winners',
+  CREATE_ROOM = 'create_room',
+  CREATE_GAME = 'create_game',
+  ADD_USER_TO_ROOM = 'add_user_to_room',
+  UPDATE_ROOM = 'update_room',
+  ADD_SHIPS = 'add_ships',
+  START_GAME = 'start_game',
+  RANDOM_ATTACK = 'randomAttack',
+  TURN = 'turn',
+  ATTACK = 'attack',
+  FINISH = 'finish',
+}
---
+++ src/types/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IPlayer {
+  index: number;
+  name: string;
+  password: string;
+}
